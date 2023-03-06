414729fc583144928008836b4b332167ffb535ec
Dara Karbassioon <karbasion.dara@gmail.com>
2023-03-06 12:43:19 -0800

    Feature 6 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from projects.models import Project
 # Create your views here.
 def list_projects(request):
@@ -7,3 +7,7 @@ def list_projects(request):
         "list_projects": list_projects
     }
     return render(request, "projects/list_projects.html", context)
+
+
+def home(request):
+    return redirect("list_projects")
--- tracker/urls.py
+++ tracker/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from projects.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("", home, name="home"),
 ]
