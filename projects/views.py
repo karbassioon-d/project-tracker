561822bfc13b20319e3d871ecd306d6d03deb1fc
Dara Karbassioon <karbasion.dara@gmail.com>
2023-03-07 07:00:04 -0800

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from projects.models import Project
+from tasks.models import Task
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -14,3 +15,13 @@ def list_projects(request):
 
 def home(request):
     return redirect("list_projects")
+
+@login_required
+def show_project(request, id):
+    show_project = Project.objects.get(id=id)
+
+    context = {
+        'show_project': show_project
+    }
+
+    return render(request, 'projects/details.html', context)
